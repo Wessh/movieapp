f1672cfcc1ec1ff89185309e3a5a842f49a05be3
Wesley <wesshbr@gmail.com>
2022-11-01 20:25:37 -0300

    Update DioServiceImp com o BaseOptions customizado

--- lib/src/services/dio_service_imp.dart
+++ lib/src/services/dio_service_imp.dart
@@ -1,10 +1,18 @@
 import 'package:dio/dio.dart';
-import 'package:dio/src/dio.dart';
 import 'package:movieapp/src/services/dio_service.dart';
+import 'package:movieapp/src/utils/key.dart';
 
 class DioServiceImp implements DioService {
   @override
   Dio getDio() {
-    return Dio();
+    return Dio(
+      BaseOptions(
+        baseUrl: 'https://api.themoviedb.org/4/',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+          'Authorization': 'Bearer $key'
+        },
+      ),
+    );
   }
 }
